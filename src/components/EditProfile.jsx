633e433cdb2266490c3d8f4c270b8533555aff83
MahiuddinZilani <zilani.cse.cou@gmail.com>
2024-10-24 03:53:07 +0600

    profile update page is made functional

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,70 +1,114 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const EditProfile = () => {
   const { user, loading } = useContext(AuthContext);
+  const navigate = useNavigate();
+
   if (loading) {
     return <div>Loading...</div>;
   }
-  console.log(user);
+
+  const handleProfileUpdateForm = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const displayName = form.displayName.value;
+    const phone = form.phone.value;
+    const address = form.address.value;
+    const photoUrl = form.photoUrl.value;
+
+    const userUpdateData = {
+      displayName,
+      phone,
+      address,
+      photoUrl,
+    };
+    // Update user data in the database
+
+    fetch(`http://localhost:5100/users/${user.email}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userUpdateData),
+    })
+      .then((res) => res.json())
+      .then(() => {
+        toast.success("Profile updated successfully");
+        navigate("/dashboard/profile");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  // console.log(user);
   return (
     <div>
       <section className="w-full p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
         <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
-          Account settings
+          Update Your Profile Information{" "}
         </h2>
-        <form>
+        <form onSubmit={handleProfileUpdateForm}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
               <label
                 className="text-gray-700 dark:text-gray-200"
-                htmlFor="username"
+                htmlFor="displayName"
               >
-                Username
+                Full Name
               </label>
               <input
-                id="username"
+                id="displayName"
                 type="text"
                 defaultValue={user.displayName}
+                name="displayName"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
             </div>
             <div>
               <label
                 className="text-gray-700 dark:text-gray-200"
-                htmlFor="emailAddress"
+                htmlFor="phone"
               >
-                Email Address
+                Phone Number
               </label>
               <input
-                id="emailAddress"
-                type="email"
+                id="phone"
+                type="tel"
+                defaultValue={user.phone}
+                name="phone"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
             </div>
             <div>
               <label
                 className="text-gray-700 dark:text-gray-200"
-                htmlFor="password"
+                htmlFor="address"
               >
-                Password
+                Address
               </label>
               <input
-                id="password"
-                type="password"
+                id="address"
+                type="address"
+                defaultValue={user.address}
+                name="address"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
             </div>
             <div>
               <label
                 className="text-gray-700 dark:text-gray-200"
-                htmlFor="passwordConfirmation"
+                htmlFor="photoUrl"
               >
                 Password Confirmation
               </label>
               <input
-                id="passwordConfirmation"
-                type="password"
+                id="photoUrl"
+                type="url"
+                defaultValue={user.photoUrl}
+                name="photoUrl"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
               />
             </div>
